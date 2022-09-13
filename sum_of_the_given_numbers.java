e24a1e17f7cb7f9bc1dc8e0434b7bdb3acbbd916
vitheshbehara <106660070+vitheshbehara@users.noreply.github.com>
2022-09-13 23:01:08 +0530

    import java.util.*; class Sample {     public static void main(String args[])     {         Scanner s=new Scanner(System.in);         int t=s.nextInt();         while(t-->0)         {             int a=s.nextInt();              int b=s.nextInt();              int c=a+b;                System.out.println(c);         }
        }
    }

---
+++ sum_of_the_given_numbers.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Sample
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        int t=s.nextInt();
+        while(t-->0)
+        {
+            int a=s.nextInt();
+             int b=s.nextInt();
+             int c=a+b;
+               System.out.println(c);
+        }
+      
+        
+    }
+}
