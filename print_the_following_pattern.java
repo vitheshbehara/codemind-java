3b2b645913bbd25c7429ec14d87254ab0410638c
vitheshbehara <106660070+vitheshbehara@users.noreply.github.com>
2022-09-13 21:16:50 +0530

    import java.util.Scanner; class Main {  public static void main(String[] args)  {    Scanner Sc=new Scanner(System.in);
             int r,i,j,k;
             int c=1;
             r=Sc.nextInt();
         
            for(i=0;i<r;i++)
            {
            for(j=r-1;j>i;j--)
            {
                System.out.print(" ");
            }
            for(k=0;k<c;k++)
            {
                System.out.print(c-j);
            }
            c=c+2;
            System.out.println();
            }
          Sc.close();
      }
     }

--- print_the_following_pattern.java
+++ print_the_following_pattern.java
@@ -1,19 +1,27 @@
 import java.util.Scanner;
 class Main
 {
-    public static void main(String[] args)
-   {
-     Scanner Sc=new Scanner(System.in);
-     int r,i,j;
-     r=Sc.nextInt();
-     for(i=1;i<=r;i++)
-     {
-       for(j=1;j<=r;j++)
-       {
-         System.out.print((char)(64+i)+" ");
-       }
-       System.out.println();
-     }
-   Sc.close();
-   }
-}
+ public static void main(String[] args)
+ {
+   Scanner Sc=new Scanner(System.in);
+
+         int r,i,j,k;
+         int c=1;
+         r=Sc.nextInt();
+     
+        for(i=0;i<r;i++)
+        {
+        for(j=r-1;j>i;j--)
+        {
+            System.out.print(" ");
+        }
+        for(k=0;k<c;k++)
+        {
+            System.out.print(c-j);
+        }
+        c=c+2;
+        System.out.println();
+        }
+      Sc.close();
+  }
+ }
