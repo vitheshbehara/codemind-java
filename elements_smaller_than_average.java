35e817a7549a8bfac9731cf10151b121794b3041
vitheshbehara <106660070+vitheshbehara@users.noreply.github.com>
2022-10-07 15:28:21 +0530

    import java.util.Scanner; class Main {     public static void main(String args[])     {         Scanner Sc=new Scanner(System.in);         int i,x[],n,sum=0,average,c=0;         n=Sc.nextInt();         x=new int[n];         for(i=0;i<n;i++)           x[i]=Sc.nextInt();         for(i=0;i<n;i++)         {             sum=sum+x[i];         }         average=sum/n;         for(i=0;i<n;i++)         {           if(x[i]<=average)             c++;         }         System.out.println(c);     } }

---
+++ elements_smaller_than_average.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+class Main
+{
+    public static void main(String args[])
+    {
+        Scanner Sc=new Scanner(System.in);
+        int i,x[],n,sum=0,average,c=0;
+        n=Sc.nextInt();
+        x=new int[n];
+        for(i=0;i<n;i++)
+          x[i]=Sc.nextInt();
+        for(i=0;i<n;i++)
+        {
+            sum=sum+x[i];
+        }
+        average=sum/n;
+        for(i=0;i<n;i++)
+        {
+          if(x[i]<=average)
+            c++;
+        }
+        System.out.println(c);
+    }
+}
