17680fdb60549b94b91babfb9a2b2460ab42ef72
vitheshbehara <106660070+vitheshbehara@users.noreply.github.com>
2023-06-05 15:38:29 +0530

    import java.util.Scanner; public class Main {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int sum=0,n;         n=sc.nextInt();         for(int i=1;i<n;i++)         {             if(n%i==0)               sum=sum+i;         }         if(sum==n)           System.out.println("True");         else           System.out.println("False");     } }

--- Perfect_Number.java
+++ Perfect_Number.java
@@ -1,23 +1,17 @@
 import java.util.Scanner;
-class Main
+public class Main
 {
-    public static int findfactor(int x)
-    {
-        int sum=0;
-        for(int i=1;i<=x/2;i++)
-        {
-            if(x%i==0)
-              sum=sum+i;
-        }
-        return sum;
-    }
     public static void main(String args[])
     {
-        Scanner Sc=new Scanner(System.in);
-        int n,result;
-        n=Sc.nextInt();
-        result=findfactor(n);
-        if(result==n)
+        Scanner sc=new Scanner(System.in);
+        int sum=0,n;
+        n=sc.nextInt();
+        for(int i=1;i<n;i++)
+        {
+            if(n%i==0)
+              sum=sum+i;
+        }
+        if(sum==n)
           System.out.println("True");
         else
           System.out.println("False");
